4a866f9e06ee8224bb4da328c07cbbddd192d546
gmbkrishna <165849996+gmbkrishna@users.noreply.github.com>
2024-08-21 21:08:52 -0400

    Add files via upload

---
+++ churnscript.sql
@@ -0,0 +1,19 @@
+CREATE TABLE [dbo].[creditchurn]
+(
+  credit_score   INT,
+  geography VARCHAR(20),
+  gender VARCHAR(20),
+  age INT,
+  tenure INT,
+  balance FLOAT,
+  num_of_products INT,
+  has_credit_card INT,
+  is_active_member INT,
+  estimated_salary FLOAT,
+  exited INT
+)
+
+SELECT*FROM [dbo].[creditchurn]
+
+
+
